673b767cf6c795706806725a0190f33dcf58ae6f
Sullivan <Sullivan.fabre@gmail.com>
2017-12-20 14:04:36 +0100

    Push project

---
+++ main.py
@@ -0,0 +1,38 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.network.urlrequest import UrlRequest
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.lang import Builder
+import json
+
+class MainPage(Screen, BoxLayout):
+    pass
+
+class ClientPage(Screen, GridLayout, Label):
+    def got_json(req):
+        result = UrlRequest('https://jsonplaceholder.typicode.com/posts')
+        bit = json.loads(result)
+        #(bit["time"]["updated"])
+        return bit
+
+    #testlol = got_json()
+
+class ScreenManagement(ScreenManager):
+    pass
+
+
+presentation = Builder.load_file("myapp.kv")
+
+class MyApp(App):
+    def build(self):
+        return presentation
+
+
+if __name__ == '__main__':
+    MyApp().run()
+
